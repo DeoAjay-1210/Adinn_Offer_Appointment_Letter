669673cc1e6886ca04b20e6db74d929afb797bec
karthika <reactdeveloper@adinn.co.in>
2026-06-17 19:01:54 +0530

    Image . removed

--- src/app/components/OfferFooter.tsx
+++ src/app/components/OfferFooter.tsx
@@ -7,10 +7,10 @@ function OfferFooterPage() {
             {/* <div style={{ maxWidth: 900, margin: "0 auto", padding: "32px 24px", fontFamily: "system-ui, sans-serif", border: '1px solid red' }}> */}
             <div className='flex justify-around gap-2 '>
                 <div className='flex justify-around ' style={{alignItems:'end', gap:'3px'}}>
-                    <div> <img src='./images/Adinn_INSLogo1.png' alt='adinnlogo' style={{ width: '50px', height: '40px' }}></img></div>
-                    <div> <img src='./images/Adinn_RuralLogo1.png' alt='adinnlogo' style={{ width: '50px', height: '30px' }}></img></div>
-                    <div> <img src='./images/Adinn_IndianOutdoorLogo1.jpg' alt='adinnlogo' style={{ width: 'auto', height: '40px' }}></img></div>
-                    <div> <img src='./images/Adinn_3mLogo1.png' alt='adinnlogo' style={{ width: '30px', height: '25px' }}></img></div>
+                    <div> <img src='/images/Adinn_INSLogo1.png' alt='adinnlogo' style={{ width: '50px', height: '40px' }}></img></div>
+                    <div> <img src='/images/Adinn_RuralLogo1.png' alt='adinnlogo' style={{ width: '50px', height: '30px' }}></img></div>
+                    <div> <img src='/images/Adinn_IndianOutdoorLogo1.jpg' alt='adinnlogo' style={{ width: 'auto', height: '40px' }}></img></div>
+                    <div> <img src='/images/Adinn_3mLogo1.png' alt='adinnlogo' style={{ width: '30px', height: '25px' }}></img></div>
                 </div>
                 <div style={{ height: 'auto', width: '0.5px', border: '0.5px solid black' }}></div>
                 <div style={{alignItems:'center', alignContent:'center'}}>
