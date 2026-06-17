1427b19f8e31cfb0ab39e9f026b021f5b9c2d8b2
karthika <reactdeveloper@adinn.co.in>
2026-06-17 19:06:07 +0530

    I caps added

--- src/app/components/OfferHead.tsx
+++ src/app/components/OfferHead.tsx
@@ -5,7 +5,7 @@ function OfferHeaderPage() {
     <div>
       {/* <div style={{ maxWidth: 900, margin: "0 auto", padding: "32px 24px", fontFamily: "system-ui, sans-serif", border: '1px solid red' }}> */}
         <div style={{ display: 'flex', justifyContent: 'end' }}>
-          <img src='/images/AdinnLogo.png'></img>
+          <img src='/Images/AdinnLogo.png'></img>
         </div>
       {/* </div> */}
 
